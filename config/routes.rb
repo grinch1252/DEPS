a4419c74eb8f2d099f5f1ef43baa3e3a249f52ca
grinch1252 <siberian.khatru1225@gmail.com>
2020-10-10 19:41:03 +0900

    user controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
   root "static_pages#home"
   get "home", to: "static_pages#home"
   get "about", to: "static_pages#about"
+  resources :users
 end
