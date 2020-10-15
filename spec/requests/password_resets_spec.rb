a6030c3f2ca87e5acbedf11b4b8174500be5f8ea
grinch1252 <siberian.khatru1225@gmail.com>
2020-10-15 11:09:24 +0900

    Rspec password-reset

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe "PasswordResets", type: :request do
     end
   end
 
-  describe "edit password" do
+  describe "GET edit_password_resets" do
     it "invalid email address" do
       get new_password_reset_path
       post password_resets_path, params: { password_reset: { email: user.email } }
@@ -56,6 +56,65 @@ RSpec.describe "PasswordResets", type: :request do
       expect(response).to redirect_to root_path
       user = assigns(:user)
       get edit_password_reset_path(user.reset_token, email: user.email)
+    end
+  end
+
+  describe "PATCH edit_password_resets" do
+    it "invalid password" do
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      user = assigns(:user)
+      get edit_password_reset_path(user.reset_token, email: user.email)
+      patch password_reset_path(user.reset_token), params: {
+        email: user.email,
+        user: {
+          password: "foobar",
+          password_confirmation: "bazfoo"
+        }
+      }
+      expect(request.fullpath).to eq "/password_resets/#{user.reset_token}"
+    end
+
+    it "empty password" do
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      user = assigns(:user)
+      get edit_password_reset_path(user.reset_token, email: user.email)
+      patch password_reset_path(user.reset_token), params: {
+        email: user.email,
+        user: {
+          password: "",
+          password_confirmation: ""
+        }
+      }
+      expect(request.fullpath).to eq "/password_resets/#{user.reset_token}"
+    end
+
+    it "expired token" do
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      user = assigns(:user)
+      user.update_attribute(:reset_sent_at, 10.hours.ago)
+      get edit_password_reset_path(user.reset_token, email: user.email)
+      patch password_reset_path(user.reset_token), params: {
+        email: user.email,
+        user: {
+          password: "testuser",
+          password_confirmation: "testuser"
+        }
+      }
+      expect(response).to redirect_to new_password_reset_url
+    end
+
+    it "valid values" do
+      post password_resets_path, params: { password_reset: { email: user.email } }
+      user = assigns(:user)
+      get edit_password_reset_path(user.reset_token, email: user.email)
+      patch password_reset_path(user.reset_token), params: {
+        email: user.email,
+        user: {
+          password: "testuser",
+          password_confirmation: "testuser"
+        }
+      }
+      expect(response).to redirect_to user_path(user)
     end
   end
 end
