8f43a985b11109023e7d526772bb0a535415928c
grinch1252 <siberian.khatru1225@gmail.com>
2020-10-19 18:43:51 +0900

    heroku set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "deps-app-77177.herokuapp.com"
+  host = "deps-77177.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
