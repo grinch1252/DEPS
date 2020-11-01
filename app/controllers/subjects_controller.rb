3853a299dfec6f858679c15d99071de21c632091
grinch1252 <siberian.khatru1225@gmail.com>
2020-11-01 16:29:31 +0900

    subject delete and show

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,14 +1,10 @@
 class SubjectsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :create, :destroy]
+  before_action :logged_in_user, only: [:index, :create, :edit, :update, :destroy]
   before_action :correct_user, only: [:destroy]
 
   def index
     @user = current_user
     @subjects = @user.subjects.page(params[:page]).per(8)
-    @subject = Subject.new
-  end
-
-  def new
     @subject = Subject.new
   end
 
@@ -20,6 +16,21 @@ class SubjectsController < ApplicationController
     else
       flash[:danger] = "Invalid information."
       redirect_to subjects_path
+    end
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def edit
+    @subject = current_user.subjects.find_by(id: params[:id])
+    if @subject.update_attributes(subject_params)
+      flash[:success] = "Subject updated."
+      redirect_to subjects_path
+    else
+      flash[:danger] = "Invalid information."
+      redirect_to edit_subject_path(@subject)
     end
   end
 
