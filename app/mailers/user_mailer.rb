32ab217f9c2cdaf46bdef4ad97651dff696d6c25
grinch1252 <siberian.khatru1225@gmail.com>
2020-11-12 23:13:52 +0900

    再デプロイ

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "アカウント有効化"
+    mail to: user.email, subject: "Account activation"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "パスワード再設定"
+    mail to: user.email, subject: "Password reset"
   end
 end
