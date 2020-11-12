f6971419f55986dc1fdba2d17ab091ed147c659a
grinch1252 <siberian.khatru1225@gmail.com>
2020-11-12 23:49:23 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,14 +2,14 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "enigmatic-hollows-19862.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "sleepy-scrubland-19570.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "587",
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
     :authentication => :plain,
-    :user_name      => ENV["SENDGRID_USERNAME"],
-    :password       => ENV["SENDGRID_PASSWORD"],
-    :domain         => "heroku.com",
     :enable_starttls_auto => true
   }
 
