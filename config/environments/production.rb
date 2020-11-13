5be3948cf28807d0263590b6ded4c7d2cf76c3cf
grinch1252 <siberian.khatru1225@gmail.com>
2020-11-13 13:58:34 +0900

    remove email authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,12 +5,13 @@ Rails.application.configure do
   host = "sleepy-scrubland-19570.herokuapp.com"
   config.action_mailer.default_url_options = { host: "powerful-forest-66977.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV[‘MAILGUN_SMTP_PORT’],
-    :address        => ENV[‘MAILGUN_SMTP_SERVER’],
-    :user_name      => ENV[‘MAILGUN_SMTP_LOGIN’],
-    :password       => ENV[‘MAILGUN_SMTP_PASSWORD’],
-    :domain         => "sleepy-scrubland-19570.herokuapp.com",
+    :address        => "smtp.sendgrid.net",
+    :port           => "587",
     :authentication => :plain,
+    :user_name      => ENV["SENDGRID_USERNAME"],
+    :password       => ENV["SENDGRID_PASSWORD"],
+    :domain         => "heroku.com",
+    :enable_starttls_auto => true
   }
 
   # Code is not reloaded between requests.
